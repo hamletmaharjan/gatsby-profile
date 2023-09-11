0d1dc96d5c10ac42e11fe2b84d1151759add4d77
hamlet maharjan <hamletmaharjan@lftechnology.com>
2023-09-11 16:51:26 +0545

    add seo component

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,8 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Hamlet Maharjan`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `Testing something and incliding hamlet maharjan in description for no reason`,
+    image: `/gatsby-icon.png`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
