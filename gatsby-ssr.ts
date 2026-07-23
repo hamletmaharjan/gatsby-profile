bb17c665a2369bc21ccd78854679202c3b9c377c
hamlet maharjan <hamletmaharjan@lftechnology.com>
2026-07-23 20:01:02 +0545

    add gatsby ssr

---
+++ gatsby-ssr.ts
@@ -0,0 +1,13 @@
+// gatsby-ssr.ts
+import React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="adsense"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9309967587775978"
+      crossOrigin="anonymous"
+    />,
+  ])
+}
